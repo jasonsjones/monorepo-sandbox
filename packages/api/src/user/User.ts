ab9c63039cebf472cdfcbd345532213ee365eca1
Jason S. Jones <jsjones96@gmail.com>
2019-02-27 19:50:57 -0800

    feat: update user model to hash password

--- packages/api/src/user/User.ts
+++ packages/api/src/user/User.ts
@@ -1,13 +1,30 @@
-import mongoose, { Document, Schema } from 'mongoose';
-import { IUser } from '../types';
-
-type UserType = IUser & Document;
+import { genSalt, hash } from 'bcrypt-nodejs';
+import mongoose, { Schema } from 'mongoose';
+import { UserModelType } from '../types';
 
 const userSchema = new Schema({
     email: String,
     password: String
 });
 
-const User = mongoose.model<UserType>('User', userSchema);
+// tslint:disable next-line
+userSchema.pre('save', function(next /*: (e: any, user?: UserModelType) => void */) {
+    const user: UserModelType = this as UserModelType;
+
+    genSalt(10, (saltErr: any, salt: string) => {
+        if (saltErr) {
+            return next(saltErr);
+        }
+        hash(user.password, salt, null, (hashErr: any, hashPwd: string) => {
+            if (hashErr) {
+                return next(hashErr);
+            }
+            user.password = hashPwd;
+            next();
+        });
+    });
+});
+
+const User = mongoose.model<UserModelType>('User', userSchema);
 
 export default User;
