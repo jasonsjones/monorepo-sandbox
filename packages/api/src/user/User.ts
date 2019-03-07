2322074c1eeab488ef9792744bb95d4b51cbc36f
Jason S. Jones <jsjones96@gmail.com>
2019-03-06 16:40:57 -0800

    fix: make user email unique for creating users

--- packages/api/src/user/User.ts
+++ packages/api/src/user/User.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:only-arrow-functions */
 import { genSalt, hash } from 'bcrypt-nodejs';
 import mongoose, { Schema } from 'mongoose';
 import { UserModelType } from '../types';
@@ -8,13 +9,12 @@ const userSchema = new Schema(
             first: String,
             last: String
         },
-        email: String,
+        email: { type: String, unique: true },
         password: String
     },
     { timestamps: true }
 );
 
-// tslint:disable next-line
 userSchema.pre('save', function(next) {
     const user: UserModelType = this as UserModelType;
 
@@ -32,11 +32,18 @@ userSchema.pre('save', function(next) {
     });
 });
 
+userSchema.post('save', function(error: any, doc: any, next: (error?: any) => void) {
+    if (error.name === 'MongoError' && error.code === 11000) {
+        next(new Error('There was a duplicate key error'));
+    } else {
+        next();
+    }
+});
+
 /*
 This is the middleware we will need to add to if we need to
 do any clean up after a user is deleted.
 
-// tslint:disable next-line
 userSchema.post('findOneAndRemove', function(doc) {
 });
 */
