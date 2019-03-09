4f04a8e12e96cebd99b0a5a4307a5280ca9c5369
Jason S. Jones <jsjones96@gmail.com>
2019-03-09 08:13:47 -0800

    refactor: update client to use hooks

--- packages/app/pages/signup.js
+++ packages/app/pages/signup.js
@@ -1,33 +1,33 @@
+import { useState } from 'react';
 import Nav from '../components/Nav';
 
-class Signup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.firstNameEl = React.createRef();
-        this.lastNameEl = React.createRef();
-        this.emailEl = React.createRef();
-        this.passwordEl = React.createRef();
-        this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: ''
-        };
-    }
+const Signup = () => {
+    const [form, setValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    });
 
-    handleSubmit = evt => {
-        evt.preventDefault();
-        this.setState(
-            {
-                firstName: this.firstNameEl.current.value,
-                lastName: this.lastNameEl.current.value,
-                email: this.emailEl.current.value,
-                password: this.passwordEl.current.value
-            },
-            () => console.log(this.state)
-        );
-
-        const query = `{version}`;
+    const handleSubmit = e => {
+        e.preventDefault();
+        const query = `mutation {
+            createUser(
+                firstName: "${form.firstName}",
+                lastName: "${form.lastName}",
+                email: "${form.email}",
+                password:"${form.password}"
+                ) {
+                _id
+                name {
+                    first
+                    last
+                }
+                email
+                password
+            }
+        }
+        `;
         fetch('http://localhost:3000/graphql', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -38,25 +38,31 @@ class Signup extends React.Component {
             .catch(err => console.log(err));
     };
 
-    render = () => {
-        return (
-            <React.Fragment>
-                <Nav />
-                <h2>Signup for an Account</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="firstName">First Name:</label>
-                    <input type="text" id="firstName" ref={this.firstNameEl} />
-                    <label htmlFor="lastName">Last Name:</label>
-                    <input type="text" id="lastName" ref={this.lastNameEl} />
-                    <br />
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" ref={this.emailEl} />
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" ref={this.passwordEl} />
-                    <button type="submit">Submit</button>
-                </form>
-            </React.Fragment>
-        );
+    const updateField = e => {
+        setValues({
+            ...form,
+            [e.target.name]: e.target.value
+        });
     };
-}
+
+    return (
+        <React.Fragment>
+            <Nav />
+            <h2>Signup for an Account</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" name="firstName" onChange={updateField} />
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" name="lastName" onChange={updateField} />
+                <br />
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" onChange={updateField} />
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" onChange={updateField} />
+                <button type="submit">Submit</button>
+            </form>
+        </React.Fragment>
+    );
+};
+
 export default Signup;
--- packages/app/pages/users.js
+++ packages/app/pages/users.js
@@ -1,48 +1,36 @@
-import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import Nav from '../components/Nav';
 
-class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: []
-        };
-    }
+const Users = () => {
+    const [users, setUsers] = useState([]);
 
-    componentDidMount() {
-        const query = `{users{_id email}}`;
+    useEffect(() => {
+        const query = `{users { _id email } }`;
         fetch('http://localhost:3000/graphql', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ query })
         })
             .then(res => res.json())
-            .then(res => {
-                this.setState({
-                    users: res.data.users
-                });
-            })
+            .then(res => setUsers(res.data.users))
             .catch(err => console.log(err));
-    }
+    }, []);
 
-    render() {
-        const { users } = this.state;
-        return (
-            <React.Fragment>
-                <Nav />
-                <h1>Users</h1>
-                {users.length > 0 && users.map(user => <p key={user._id}>{user.email}</p>)}
-                <style jsx>{`
-                    h1 {
-                        font-family: 'Arial';
-                        margin: 0;
-                        padding: 0;
-                        color: #1f3c88;
-                    }
-                `}</style>
-            </React.Fragment>
-        );
-    }
-}
+    return (
+        <React.Fragment>
+            <Nav />
+            <h1>Users</h1>
+            {users.length > 0 && users.map(user => <p key={user._id}>{user.email}</p>)}
+            <style jsx>{`
+                h1 {
+                    font-family: 'Arial';
+                    margin: 0;
+                    padding: 0;
+                    color: #1f3c88;
+                }
+            `}</style>
+        </React.Fragment>
+    );
+};
 
 export default Users;
