dcd4c97d32d9213cce8dfd18e06f63da7ae0f9c1
Jason S. Jones <jsjones96@gmail.com>
2019-03-18 17:01:20 -0700

    fix: inject graphql server to get gql path

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,15 +1,18 @@
+import { ApolloServer } from 'apollo-server-express';
 import debug from 'debug';
 import AppProvider from './config/AppProvider';
+import { GraphqlServerProvider } from './config/GraphqlServer';
 
 const log = debug('app');
 
 const PORT = process.env.PORT || 3000;
 
-const startServer = async () => {
+const startServer = async (gqlServer: ApolloServer) => {
     const app = await AppProvider.getInstance();
     app.listen(PORT, () => {
         log(`App is running on http://localhost:${PORT}/api`);
+        log(`Graphql server is running on http://localhost:${PORT}${gqlServer.graphqlPath}`);
     });
 };
 
-startServer();
+startServer(GraphqlServerProvider.getInstance());
