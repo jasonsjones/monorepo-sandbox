1dd76edd498109323fe2d65a7c7677af477affd4
Jason S. Jones <jsjones96@gmail.com>
2019-03-22 16:59:26 -0700

    feat: add initial AuthContext

--- packages/app/components/Layout.js
+++ packages/app/components/Layout.js
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import Nav from './Nav';
-const Layout = ({ children }) => (
-    <React.Fragment>
-        <Nav />
-        {children}
-    </React.Fragment>
-);
+import AuthContext from '../context/AuthContext';
+
+const Layout = ({ children }) => {
+    const [authUser, setAuthUser] = useState(null);
+    const [token, setToken] = useState('');
+    return (
+        <AuthContext.Provider value={{ authUser, token }}>
+            <Nav />
+            {children}
+        </AuthContext.Provider>
+    );
+};
 
 export default Layout;
