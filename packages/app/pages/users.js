31a27cadefb3877713114da9361eaa5ebe104d9b
Jason S. Jones <jsjones96@gmail.com>
2019-04-24 16:47:48 -0700

    fix: make fetch call only if user is auth'ed

--- packages/app/pages/users.js
+++ packages/app/pages/users.js
@@ -3,34 +3,42 @@ import AuthContext from '../context/AuthContext';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
+    const [errMsg, setErrMsg] = useState(null);
     const authCtx = useContext(AuthContext);
 
     useEffect(() => {
-        const query = `{users { _id email } }`;
-        fetch('http://localhost:3000/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${authCtx.token}`
-            },
-            body: JSON.stringify({ query })
-        })
-            .then(res => res.json())
-            .then(res => {
-                const { users } = res.data;
-                if (users) {
-                    setUsers(users);
-                } else {
-                    setUsers([]);
-                }
+        if (!authCtx.token) {
+            setErrMsg('Must be authenticated to view this resource');
+        } else {
+            const query = `{users { _id email } }`;
+            fetch('http://localhost:3000/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${authCtx.token}`
+                },
+                body: JSON.stringify({ query })
             })
-            .catch(err => console.log(err));
+                .then(res => res.json())
+
+                .then(res => {
+                    const { users } = res.data;
+                    if (users) {
+                        setUsers(users);
+                    } else {
+                        setUsers([]);
+                    }
+                    setErrMsg(null);
+                })
+                .catch(err => console.log(err));
+        }
     }, []);
 
     return (
         <React.Fragment>
             <h1>Users</h1>
             {users.length > 0 && users.map(user => <p key={user._id}>{user.email}</p>)}
+            {errMsg && <p>{errMsg}</p>}
         </React.Fragment>
     );
 };
