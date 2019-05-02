75b2982cf220d83f07a0294fe25b8a6919fc3bda
Jason S. Jones <jsjones96@gmail.com>
2019-05-02 13:20:39 -0700

    refactor: db module

---
+++ packages/api/src/config/db.ts
@@ -0,0 +1,55 @@
+import debug from 'debug';
+import mongoose, { Connection } from 'mongoose';
+import config from './config';
+
+const log = debug('db');
+const error = debug('db:error');
+
+const getDbName = (): string => {
+    return config.env !== 'testing' ? 'sandbox-dev' : 'sandbox-test';
+};
+
+const buildConnectionString = (): string => {
+    return `${config.db.baseUri}/${getDbName()}?${config.db.options}`;
+};
+
+const attachHandlers = (connection: Connection): void => {
+    connection.once('open', () => {
+        log(`Connected to ${getDbName()} on a MongoDB Atlas cluster`);
+    });
+
+    connection.on('error', () => {
+        error('connection error');
+    });
+
+    connection.on('disconnected', () => {
+        log(`db disconnected`);
+    });
+
+    process.on('SIGINT', () => {
+        connection.close(() => {
+            log('default connection closed via app termination');
+            process.exit(0);
+        });
+    });
+
+    process.once('SIGUSR2', () => {
+        connection.close(() => {
+            log('default connection closed via ts-node-dev restart');
+            process.kill(process.pid, 'SIGUSR2');
+        });
+    });
+};
+
+export const dbConnect = (): Connection => {
+    mongoose.connect(buildConnectionString(), {
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useNewUrlParser: true
+    });
+
+    const connection = mongoose.connection;
+    attachHandlers(connection);
+
+    return connection;
+};
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,14 +1,14 @@
 import { ApolloServer } from 'apollo-server-express';
 import debug from 'debug';
 import app from './config/app';
-import DatabaseManager from './config/DatabaseManager';
+import { dbConnect } from './config/db';
 import gqlServer from './config/graphqlServer';
 
 const log = debug('app');
 const PORT = process.env.PORT || 3000;
 
 const startServer = (server: ApolloServer): void => {
-    DatabaseManager.connect();
+    dbConnect();
     app.listen(PORT, () => {
         log(`App is running on http://localhost:${PORT}/api`);
         log(`Graphql server is running on http://localhost:${PORT}${server.graphqlPath}`);
