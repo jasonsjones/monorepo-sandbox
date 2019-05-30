6a4c7d0e41c9e063d2eecc5f4c81909a5142d14a
Jason S. Jones <jsjones96@gmail.com>
2019-05-29 20:18:35 -0700

    refactor: remove unused code

--- packages/api/src/config/addRoutes.ts
+++ packages/api/src/config/addRoutes.ts
@@ -1,10 +1,8 @@
 import { Application } from 'express';
 import IndexRouter from '../index/IndexRouter';
-import userRouter from '../user/userRouter';
 
 const addRoutes = (app: Application): void => {
     app.use('/api', IndexRouter.getInstance());
-    app.use('/api/users', userRouter);
 };
 
 export default addRoutes;
