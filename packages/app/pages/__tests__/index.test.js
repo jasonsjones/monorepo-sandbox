bbbb013ed395404fa2f7ad3199fae697958ec07f
Jason S. Jones <jsjones96@gmail.com>
2019-07-01 19:44:39 -0700

    test: add test to app index page

--- packages/app/pages/__tests__/index.test.js
+++ packages/app/pages/__tests__/index.test.js
@@ -3,7 +3,13 @@ import { render, cleanup } from '@testing-library/react';
 import Home from '../index';
 
 afterEach(cleanup);
+
 describe('Index page', () => {
+    it('renders a h1', () => {
+        const { container } = render(<Home />);
+        expect(container.firstChild.nodeName).toBe('H1');
+    });
+
     it('renders Welcome text', () => {
         const { getByText } = render(<Home />);
         expect(getByText(/Welcome/)).toBeTruthy();
