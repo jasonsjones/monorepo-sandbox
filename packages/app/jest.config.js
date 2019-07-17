c588ce2f048599e490debd5b72e15c235e7678f6
Jason S. Jones <jsjones96@gmail.com>
2019-07-16 19:54:32 -0700

    test: add style mocks for css imports

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
+    },
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
 };
