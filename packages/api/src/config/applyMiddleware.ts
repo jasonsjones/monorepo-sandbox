afbff29a3ab4adc89a79b581ff133db8721544e7
Jason S. Jones <jsjones96@gmail.com>
2019-08-17 20:18:18 -0700

    chore: remove cors

--- packages/api/src/config/applyMiddleware.ts
+++ packages/api/src/config/applyMiddleware.ts
@@ -1,5 +1,4 @@
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import express, { Application, NextFunction, Response } from 'express';
 import morgan from 'morgan';
 import { verifyToken } from '../auth/authUtils';
@@ -47,7 +46,6 @@ const applyMiddleware = (app: Application): void => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
-    app.use(cors());
 
     app.use(addUserToRequest);
 
