a9dab1bb2e58ca7d3b2d25dee27afa1541938d80
Jason S. Jones <jsjones96@gmail.com>
2019-08-21 19:40:18 -0700

    feat: add email verification to user profile

--- packages/app/components/User/UserProfile.js
+++ packages/app/components/User/UserProfile.js
@@ -12,6 +12,15 @@ const doUpdate = (query, variables) => {
 };
 */
 
+const doResend = query => {
+    return fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query })
+    }).then(res => res.json());
+};
+
 const Profile = props => {
     const { contextUser, isEditMode } = props;
     const [editedUser, setEditedUser] = useState({
@@ -48,6 +57,16 @@ const Profile = props => {
             [e.target.id]: e.target.value
         });
     };
+
+    const handleResend = () => {
+        console.log('resending email verification message...');
+        const query = `query { resendEmailVerification }`;
+        doResend(query).then(data => {
+            console.log(data);
+            console.log('check your email...');
+        });
+    };
+
     return (
         <React.Fragment>
             <div className="profile-container">
@@ -89,16 +108,19 @@ const Profile = props => {
                     ) : (
                         <p className="email">{editedUser.email}</p>
                     )}
-                    {contextUser.isEmailVerified && (
+                    {contextUser.isEmailVerified && !isEditMode && (
                         <div className="email-verify-display">
                             <p>Email verified</p>
-                            <img src="https:icon.now.sh/done/18/00aa00" alt="done icon" />
+                            <img src="https:icon.now.sh/done/28/00aa00" alt="done icon" />
                         </div>
                     )}
-                    {!contextUser.isEmailVerified && (
+                    {!contextUser.isEmailVerified && !isEditMode && (
                         <div className="email-verify-display">
                             <p>Email verified</p>
-                            <img src="https:icon.now.sh/close/18/aa0000" alt="close icon" />
+                            <img src="https:icon.now.sh/close/28/aa0000" alt="close icon" />
+                            <button type="button" className="resend-link" onClick={handleResend}>
+                                Resend
+                            </button>
                         </div>
                     )}
                     <p className="id">{contextUser._id}</p>
@@ -112,6 +134,20 @@ const Profile = props => {
                 }
                 p {
                     margin: 0;
+                }
+                button {
+                    height: 1.75rem;
+                    width: 60px;
+                    font-size: 0.75rem;
+                    background-color: #fff;
+                    border-color: #022c43;
+                    color: #022c43;
+                    border-radius: 5px;
+                }
+
+                button:hover {
+                    background-color: #ddd;
+                    cursor: pointer;
                 }
                 .info {
                     margin-left: 2rem;
@@ -139,7 +175,7 @@ const Profile = props => {
                     align-items: center;
                 }
                 .email-verify-display img {
-                    margin: 0 10px;
+                    margin: 0 40px 0 5px;
                 }
                 @media only screen and (min-device-width: 375px) and (max-device-width: 667px) {
                     .profile-container {
