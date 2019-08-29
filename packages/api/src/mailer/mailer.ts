703d47d02fa833cfd4d3a6b82f4231b50a1fe79c
Jason S. Jones <jsjones96@gmail.com>
2019-08-29 08:53:01 -0700

    refactor: move html tmpl to templates file

--- packages/api/src/mailer/mailer.ts
+++ packages/api/src/mailer/mailer.ts
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer';
 import config from '../config/config';
 import { IUser } from '../types';
+import { getEmailVerificatonTemplate, getPasswordResetTemplate } from './templates';
 
 class Mailer {
     public sendVerificatonEmail = async (user: IUser): Promise<any> => {
@@ -19,48 +20,7 @@ Please verify your email. Thank you!
 
 http://localhost:4200/verifyemail?token=${user.emailVerificationToken}
                 `,
-                html: `
-<style>
-    body {
-        background-color: #eee;
-        font-family: sans-serif;
-    }
-    .container {
-        max-width: 560px;
-        margin: 0 auto;
-        background-color: #fff;
-        border: 1px solid #ccc;
-        border-radius: 10px;
-        font-size: 16px;
-        font-weight: 300;
-    }
-    .banner {
-        height: 60px;
-        background-color: #00aa00;
-        background: rgb(2, 0, 36);
-        background: linear-gradient(90deg, #020024 0%, #022c43 40%, #064f77 100%);
-    }
-    .banner p {
-        color: #fff;
-        text-align: center;
-        font-size: 1.4rem;
-        margin: 0;
-        padding-top: 15px;
-    }
-    .text {
-        padding: 0 30px
-    }
-</style>
-<div class="container" style="">
-    <div class="banner"><p>Thank you for registering</p></div>
-    <p class="text">Thanks for using Orion Labs! Please confirm your email address by clicking on the link below. We'll communicate with you from time to time via email so it's important that we have an up-to-date email address on file.</p>
-    <p class= "text"><a href="http://localhost:4200/verifyemail?token=${user.emailVerificationToken}">http://localhost:4200/verifyemail?token=${user.emailVerificationToken}</a></p>
-    <p class="text">If you did not sign up for a Orion Labs account please disregard this email.</p>
-    <p class="text">Thank you!</p>
-    <p class="text">&mdash; The Orion Labs Team</p>
-
-</div>
-                `
+                html: getEmailVerificatonTemplate(user)
             });
         }
     };
@@ -82,7 +42,7 @@ If you did, then you will need to click on the below link and change your passwo
 
 http://localhost:4200/changepassword?token=${user.passwordResetToken}
                 `,
-                html: ``
+                html: getPasswordResetTemplate(user)
             });
         }
     };
---
+++ packages/api/src/mailer/templates.ts
@@ -0,0 +1,85 @@
+import { IUser } from '../types';
+
+const styles = `
+<style>
+    body {
+        background-color: #eee;
+        font-family: sans-serif;
+    }
+    .container {
+        max-width: 560px;
+        margin: 0 auto;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        font-size: 16px;
+        font-weight: 300;
+    }
+    .banner {
+        height: 60px;
+        background-color: #00aa00;
+        background: rgb(2, 0, 36);
+        background: linear-gradient(90deg, #020024 0%, #022c43 40%, #064f77 100%);
+    }
+    .banner p {
+        color: #fff;
+        text-align: center;
+        font-size: 1.4rem;
+        margin: 0;
+        padding-top: 15px;
+    }
+    .text {
+        padding: 0 30px
+    }
+    .btn {
+        display: block;
+        height: 40px;
+        width: 150px;
+        font-size: 1rem;
+        background-color: #022c43;
+        color: #fff;
+        border-radius: 5px;
+        margin: 0 auto;
+        padding-top: 20px;
+        text-align: center;
+        text-decoration: none;
+    }
+</style>
+`;
+
+export const getEmailVerificatonTemplate = (user: IUser) => {
+    return `
+${styles}
+<div class="container">
+    <div class="banner"><p>Thank you for registering</p></div>
+    <p class="text">Thanks for using Orion Labs! Please confirm your email address by clicking on the link below. We'll communicate with you from time to time via email so it's important that we have an up-to-date email address on file.</p>
+    <a class="btn" href="http://localhost:4200/verifyemail?token=${user.emailVerificationToken}">Verify Email</a>
+    <p class="text">or click on the below link:</p>
+    <p class="text"><a href="http://localhost:4200/verifyemail?token=${user.emailVerificationToken}">http://localhost:4200/verifyemail?token=${user.emailVerificationToken}</a></p>
+    <p class="text">If you did not sign up for a Orion Labs account please disregard this email.</p>
+    <p class="text">Thank you!</p>
+    <p class="text">&mdash; The Orion Labs Team</p>
+</div>
+<p style="text-align: center; color: #aaa; font-size: .75rem">Orion Labs, LLC, 1234 Main St., AnyTown WA, 98111</p>
+    `;
+};
+
+export const getPasswordResetTemplate = (user: IUser) => {
+    return `
+${styles}
+<div class="container">
+    <div class="banner"><p>Password Reset</p></div>
+    <p class="text">
+        You recently requested to reset your password with Orion Labs.  If you did not initiate this request, no action is required.
+        If you did, then you will need to click on the below link and change your password.  This link is only valid for the next 2 hours.
+    </p>
+    <a class="btn" href="http://localhost:4200/changepassword?token=${user.passwordResetToken}">Reset Password</a>
+    <p class="text">or click on the below link:</p>
+    <p class="text"><a href="http://localhost:4200/changepassword?token=${user.passwordResetToken}">http://localhost:4200/changepassword?token=${user.passwordResetToken}</a></p>
+    <p class="text">If you did not request a password reset for your Orion Labs account please disregard this email.</p>
+    <p class="text">Thank you!</p>
+    <p class="text">&mdash; The Orion Labs Team</p>
+</div>
+<p style="text-align: center; color: #aaa; font-size: .75rem">Orion Labs, LLC, 1234 Main St., AnyTown WA, 98111</p>
+    `;
+};
