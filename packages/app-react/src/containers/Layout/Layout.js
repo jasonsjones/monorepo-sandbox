b9f12d59ca7b34c5f55f00343179bad67503b3f3
Jason S. Jones <jsjones96@gmail.com>
2019-10-15 14:28:08 -0700

    refactor: rename webapp package

--- packages/app-react/src/containers/Layout/Layout.js
+++ packages/app-react/src/containers/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Nav from '../../components/Nav/';
-import Banner from '../../components/Banner/';
+import Nav from '../../components/Nav';
+import Banner from '../../components/Banner';
 import './Layout.css';
 
 const Footer = () => {
