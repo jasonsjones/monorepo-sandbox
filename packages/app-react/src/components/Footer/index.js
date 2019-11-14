0ce505f4cfd26046a287143539f39a221c9709ab
Jason S. Jones <jsjones96@gmail.com>
2019-11-13 18:08:22 -0800

    fix: add hover color change to social links

--- packages/app-react/src/components/Footer/index.js
+++ packages/app-react/src/components/Footer/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
 
 const Footer = () => {
+    const [githubIconColor, setGithubIconColor] = useState('ccc');
+    const [twitterIconColor, setTwitterIconColor] = useState('ccc');
+
     return (
         <div className="site-footer_content">
             <section className="site-footer_about">
@@ -14,7 +17,10 @@ const Footer = () => {
             <section className="site-footer_connect">
                 <h3>Connect</h3>
                 <ul>
-                    <li>
+                    <li
+                        onMouseOver={() => setGithubIconColor('fff')}
+                        onMouseOut={() => setGithubIconColor('ccc')}
+                    >
                         <a
                             href="https://github.com/jasonsjones"
                             target="_blank"
@@ -23,13 +29,16 @@ const Footer = () => {
                         >
                             <img
                                 className="social-icon"
-                                src="https:icon.now.sh/github/14/fff"
+                                src={`https:icon.now.sh/github/14/${githubIconColor}`}
                                 alt="github account"
                             />
                             <span className="social-name">github</span>
                         </a>
                     </li>
-                    <li>
+                    <li
+                        onMouseOver={() => setTwitterIconColor('fff')}
+                        onMouseOut={() => setTwitterIconColor('ccc')}
+                    >
                         <a
                             href="https://twitter.com/jsj0nes"
                             target="_blank"
@@ -38,7 +47,7 @@ const Footer = () => {
                         >
                             <img
                                 className="social-icon"
-                                src="https:icon.now.sh/twitter/14/fff"
+                                src={`https:icon.now.sh/twitter/14/${twitterIconColor}`}
                                 alt="twitter account"
                             />
                             <span className="social-name">twitter</span>
