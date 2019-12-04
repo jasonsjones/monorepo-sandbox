56cbac5594c8ab998d45915293e907a81c438e83
Jason S. Jones <jsjones96@gmail.com>
2019-12-04 10:10:42 -0800

    refactor: rename layout js file to index.js

--- packages/app-react/src/App.js
+++ packages/app-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Layout from './containers/Layout/Layout';
+import Layout from './containers/Layout';
 import Signup from './containers/Signup';
 import Login from './containers/Login';
 import Home from './components/Home';
