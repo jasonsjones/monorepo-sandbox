149c24496992a3801b0c743bb8f08e275cfd818d
Jason S. Jones <jsjones96@gmail.com>
2019-12-19 10:38:01 -0800

    fix: update email confirmation url in email template

--- packages/api-postgres/src/services/mailer/templates.ts
+++ packages/api-postgres/src/services/mailer/templates.ts
@@ -55,7 +55,7 @@ ${styles}
     <p class="text">Thanks for using Orion Labs! Please confirm your email address by clicking on the link below. We'll communicate with you from time to time via email so it's important that we have an up-to-date email address on file.</p>
     <a class="btn" href="${url}/confirm-email?token=${user.emailVerificationToken}">Verify Email</a>
     <p class="text">or click on the below link:</p>
-    <p class="text"><a href="${url}/confirm-email?token=${user.emailVerificationToken}">${url}/confirm-email?token=${user.emailVerificationToken}</a></p>
+    <p class="text"><a href="${url}/confirm-email/${user.emailVerificationToken}">${url}/confirm-email/${user.emailVerificationToken}</a></p>
     <p class="text">If you did not sign up for a Orion Labs account please disregard this email.</p>
     <p class="text">Thank you!</p>
     <p class="text">&mdash; The Orion Labs Team</p>
@@ -70,6 +70,6 @@ Please verify your email. Thank you!
 // this email will include the token -- ${user.emailVerificationToken}
 // or maybe a link to some RESTful endpoint:
 
-${url}/confirm-email?token=${user.emailVerificationToken}
+${url}/confirm-email/${user.emailVerificationToken}
     `;
 };
