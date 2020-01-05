0852aabc69ecf1f3a65bfdba69361547722fbb4d
Jason S. Jones <jsjones96@gmail.com>
2020-01-04 16:47:10 -0800

    test: add mailer test

--- packages/api-postgres/src/__tests__/mailer.test.ts
+++ packages/api-postgres/src/__tests__/mailer.test.ts
@@ -15,13 +15,24 @@ afterAll(async () => {
 });
 
 describe('Mailer', () => {
+    let user: User;
+
+    beforeAll(async () => {
+        user = await UserService.createUser('Oliver', 'Queen', 'oliver@qc.com', '123456');
+    });
+
     it('exists', () => {
         expect(Mailer).toBeTruthy();
     });
 
     it('sends default verification email', async () => {
-        const user = await UserService.createUser('Oliver', 'Queen', 'oliver@qc.com', '123456');
         const email = await Mailer.sendVerificationEmail('http://localhost:4201', user as User);
+        expect(email).toHaveProperty('envelope');
+        expect(email).toHaveProperty('messageId');
+    });
+
+    it('sends password reset email', async () => {
+        const email = await Mailer.sendPasswordResetEmail('http://localhost:4201', user as User);
         expect(email).toHaveProperty('envelope');
         expect(email).toHaveProperty('messageId');
     });
