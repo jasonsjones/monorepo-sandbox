9f30896203c1a2d648cef199a30f841be14f8b4b
Jason S. Jones <jsjones96@gmail.com>
2020-08-25 12:54:44 -0700

    refactor: update Button & TextField to use tailwind css

--- packages/app-react/src/components/Common/Button/index.js
+++ packages/app-react/src/components/Common/Button/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
-const FormButton = styled.button`
-    min-height: 2.5rem;
-    min-width: 80px;
-    font-size: 1rem;
-    padding: 0.75rem;
-    background-color: #022c43;
-    color: #ccc;
-    border-radius: 5px;
-    cursor: pointer;
-
-    &:hover {
-        background-color: #115173;
-    }
-`;
-
-const Button = props => {
+const Button = (props) => {
     return (
-        <FormButton onClick={props.clickHandler} type={props.type}>
+        <button
+            type={props.type}
+            className="py-3 px-4 bg-purple-700 font-semibold rounded-md text-gray-200 hover:bg-purple-800"
+        >
             {props.text}
-        </FormButton>
+        </button>
     );
 };
 
