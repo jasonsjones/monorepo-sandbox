6bb5a9e4927dac3eaea5c65e670a493d42642660
Jason S. Jones <jsjones96@gmail.com>
2020-08-25 09:08:24 -0700

    refactor: login form container to use tailwind css

--- packages/app-react/src/containers/Login/index.js
+++ packages/app-react/src/containers/Login/index.js
@@ -1,53 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from '@emotion/styled';
 import LoginForm from '../../components/LoginForm';
-
-const Container = styled.div`
-    max-width: 1280px;
-    margin: 20px auto 0;
-
-    & h2 {
-        text-align: center;
-    }
-`;
-
-const LoginFormContainer = styled.div`
-    max-width: 460px;
-    margin: 1.25rem auto;
-
-    @media only screen and (min-device-width: 375px) and (max-device-width: 667px) {
-        padding: 0 1.25rem;
-    }
-`;
-
-const ForgotPasswordContainer = styled.div`
-    margin: 1.25rem 0 1rem;
-    border-top: 1px #ddd solid;
-    padding-top: 1.25rem;
-
-    & a {
-        text-decoration: none;
-        color: #022c43;
-        font-size: 0.9rem;
-    }
-
-    @media only screen and (min-device-width: 375px) and (max-device-width: 667px) {
-        margin: 2rem 0 1rem;
-    }
-`;
 
 const Login = ({ history }) => {
     return (
-        <Container>
-            <h2>Login</h2>
-            <LoginFormContainer>
+        <div className="max-w-4xl mt-6 mx-auto">
+            <h2 className="text-3xl font-semibold text-center text-purple-900">Login</h2>
+            <div className="max-w-md mx-auto px-6 sm:px-0">
                 <LoginForm history={history} />
-                <ForgotPasswordContainer>
-                    <Link to="/forgotpassword">Forgot your password?</Link>
-                </ForgotPasswordContainer>
-            </LoginFormContainer>
-        </Container>
+                <div className="mt-4 mb-4 pt-6 border-t-2">
+                    <Link to="/forgotpassword" className="text-sm text-purple-900">
+                        Forgot your password?
+                    </Link>
+                </div>
+            </div>
+        </div>
     );
 };
 
