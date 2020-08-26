a9482ec21ace98065ba3782eaba4041fdff96d2a
Jason S. Jones <jsjones96@gmail.com>
2020-08-25 21:30:13 -0700

    style: reduce vertical padding on button

--- packages/app-react/src/components/Common/Button/index.js
+++ packages/app-react/src/components/Common/Button/index.js
@@ -4,7 +4,7 @@ const Button = (props) => {
     return (
         <button
             type={props.type}
-            className="py-3 px-4 bg-purple-700 font-semibold rounded-md text-gray-200 hover:bg-purple-800"
+            className="py-2 px-4 bg-purple-700 font-semibold rounded-md text-gray-200 hover:bg-purple-800"
         >
             {props.text}
         </button>
